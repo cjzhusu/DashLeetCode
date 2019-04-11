a54d40949ae5b4217a3f96f9734afa7e3cac48fd
cjzhusu <cjzhusu@163.com>
2019-04-11 22:14:07 +0800

    add todo

---
+++ src/main/java/com/github/cjzhusu/dashleetcode/algorithm/LongestPalindromicSubstring.java
@@ -0,0 +1,21 @@
+package com.github.cjzhusu.dashleetcode.algorithm;
+
+/**
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * Example 2:
+ * <p>
+ * Input: "cbbd"
+ * Output: "bb"
+ */
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        // TODO
+        return "";
+    }
+}
